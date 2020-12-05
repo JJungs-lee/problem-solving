eac194b54f2e27084308ae8dd152b173fd876581
JJung-lee <loveljhs2@gmail.com>
2020-12-05 23:38:22 +0900

    20.12.05 - code change

--- BOJ/1920_수 찾기.cpp
+++ BOJ/1920_수 찾기.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 int cnt;
 int arr[100001];
 
-int findAnswer(int findNum){
-
+int findAnswer(int findNum) {
 	int start = 0;
 	int end = cnt - 1;
 	int mid = (start + end) / 2;
 
-	for(int j = 0; j < 5000; ++j){
-		if(arr[mid] == findNum) return true;
-		if(arr[mid] < findNum) start = mid + 1;
-		else end = mid - 1;
-
+	while (start <= end) {
 		mid = (start + end) / 2;
+		if (arr[mid] == findNum)
+			return true;
+		else if (arr[mid] < findNum)
+			start = mid + 1;
+		else
+			end = mid - 1;
 	}
 
 	return false;
 }
 
-int main(){
-
-
+int main() {
 	int findCnt;
 	int findNum;
-	cin >> cnt;
-
-	for(int i = 0; i < cnt; ++i) cin >> arr[i];
+	
+	scanf("%d", &cnt);
+	for (int i = 0; i < cnt; ++i) 
+		scanf("%d", &arr[i]);
 
 	sort(arr, arr + cnt);
 
-	cin >> findCnt;
+	scanf("%d", &findCnt);
 
-	for(int j = 0; j < findCnt; ++j){
-		cin >> findNum;
-		cout << findAnswer(findNum) << endl;
+	for (int j = 0; j < findCnt; ++j) {
+		scanf("%d", &findNum);
+		printf("%d\n",findAnswer(findNum));
 	}
 
 	return 0;
