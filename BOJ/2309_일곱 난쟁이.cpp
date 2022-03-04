0712442e0045c0821c71eff1ee6accb6c1482623
JJung-lee <loveljhs2@gmail.com>
2022-03-04 23:00:23 +0900

    22.03.04 - 풀이 수정

--- BOJ/2309_일곱 난쟁이.cpp
+++ BOJ/2309_일곱 난쟁이.cpp
@@ -5,17 +5,22 @@ using namespace std;
 int main(){
 	int sum = 0;
 	int arr[9];
-	for(int i = 0; i<9; ++i){
+	for (int i = 0; i < 9; ++i) {
 		cin >> arr[i];
 		sum += arr[i];
 	}
-	for(int i = 0; i<8; ++i){
-		for(int j = 1; j<9; ++j){
-			if(arr[i] + arr[j] == sum - 100) arr[i] = arr[j] = 200;
+	for (int i = 0; i < 8; ++i) {
+		for (int j = i + 1; j < 9; ++j) {
+			if (arr[i] + arr[j] == sum - 100) {
+				arr[i] = arr[j] = 200;
+				goto ans;
+			}
 		}
 	}
+ans:
 	sort(arr, arr + 9);
-	for(int i = 0; i<7; ++i) cout << arr[i] << endl;
+	for(int i = 0; i < 7; ++i)
+		cout << arr[i] << endl;
 
 	return 0;
 }
