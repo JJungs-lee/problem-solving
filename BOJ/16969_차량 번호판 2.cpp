4a3660d2df515a61b0e63930b653074d0683d42f
JJung-lee <loveljhs2@gmail.com>
2022-03-07 23:11:15 +0900

    22.03.07 - 차량 번호판

---
+++ BOJ/16969_차량 번호판 2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+#define NUM 1
+#define ALPHA 2
+#define DEFAULT_NUM 10
+#define DEFAUTL_ALPHA 26
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int num_cnt = DEFAULT_NUM;
+	int alpha_cnt = DEFAUTL_ALPHA;
+	long ret = 1;
+	int status = 0;
+	string str;
+	cin >> str;
+
+	for (auto c: str) {
+		switch(c) {
+			case 'c': {
+				status == ALPHA? ret *= (DEFAUTL_ALPHA -1): ret *= DEFAUTL_ALPHA;
+				status = ALPHA;
+				break;
+			}
+			case 'd': {
+				status == NUM? ret *= (DEFAULT_NUM - 1): ret *= DEFAULT_NUM;
+				status = NUM;
+				break;
+			}
+		}
+		ret %= 1000000009;
+	}
+	cout << ret << endl;
+	return 0;
+}
