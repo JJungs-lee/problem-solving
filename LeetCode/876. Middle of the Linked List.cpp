48d87a6146813f85348699d770c7eec0a9071387
JJung-lee <loveljhs2@gmail.com>
2022-04-01 22:36:51 +0900

    leetcode - Algorithm Day5: Two Pointers

---
+++ LeetCode/876. Middle of the Linked List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* middleNode(ListNode* head) {
+		ListNode* slow = head;
+		ListNode* fast = head;
+
+		while (fast->next != nullptr) {
+			slow = slow->next;
+			fast = fast->next;
+			if (fast->next != nullptr)
+				fast = fast->next;
+			else
+				return slow;
+		}
+		return slow;
+	}
+};
